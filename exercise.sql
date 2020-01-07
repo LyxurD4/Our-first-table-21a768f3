1805455990ada47940eb1f4dcc6a716540b52f89
LyxurD4 <mdoes@roc-dev.com>
2020-01-07 10:41:36 +0100

    Uitwerking van de exercise

---
+++ exercise.sql
@@ -0,0 +1,5 @@
+use sterrenstelsel;
+create table planeten (mercurius varchar(10), venus varchar(10), jupiter varchar(10), venus varchar(10), aarde varchar(10), mars varchar(10), jupiter varchar(10), saturnus varchar(10), uranus varchar(10), neptunus varchar(10));
+
+
+
